5c7fac598eda1285c27307c4f5c7083911e1cad2
Cihakarmss <cihadkrimli@gmail.com>
2025-01-14 02:50:11 +0400

    Add files via upload

---
+++ TASK JS/assets/rc.js
@@ -0,0 +1,18 @@
+let score =(Number(prompt("qiymet elave et:" )))
+if(score>=90){
+    alert("Elacidir");
+}
+    
+else if(score<90 && score>=70 ){
+    alert("yaxsi")
+}
+    
+else if(score<70 && score>50){
+    alert("kafi")
+}
+    
+else if(score<=50){
+    alert("Kesr")
+}
+    
+
