304c5382ee486fe6fad6808e5d6615ed6b2e4910
Cihakarmss <cihadkrimli@gmail.com>
2025-02-12 20:58:24 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let basliq =document.getElementById("basliq")
+console.log(basliq);
+
+let metnler=document.getElementsByClassName("metnler")
+console.log(metnler);
+
+let button=document.getElementsByTagName("ul")
+console.log(button);
+
+let qutu=document.querySelector(".qutu")
+console.log(qutu);
+
+let butunmetnler=document.querySelectorAll(".metn")
+console.log(butunmetnler);
+
+function metni_deyis(metn) {
+    document.getElementById("metni deyis").textContent="goat-Ronaldo"
+}
+
+function rengi_deyis(color) {
+    document.querySelector(".change_color").css
+}
+
