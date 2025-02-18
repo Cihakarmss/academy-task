49c70122f2f43f2f83dae8fcb86e2b53b620374c
Cihakarmss <cihadkrimli@gmail.com>
2025-02-18 17:48:58 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,24 @@
-// const arr1=[1,2,3,4,5];
-// const arr2=[6,7,8,9,];
-// const arr3=[0,...arr1,...arr2];
-// console.log(arr3);
+const input = document.querySelector('input');
+const add = document.querySelector('.add');
+const list=document.querySelector('.list')
 
-// function avarageGrade(...numbers) {
-//     if (numbers.length===0) {
-//         return null;
-        
-//     }
-//     else{
-//         let sum=0;
-//         for (let number of numbers) {
-//             sum+=number;
-//         }
-//         return sum/numbers.length;}
-// }
-// console.log(avarageGrade())
+add.addEventListener('click',function () {
+    let li = document.createElement('li');
+    let inputValue=input.value
+    if (inputValue.trim()!=='') {
+        li.innerHTML = `${inputValue} <button class="delete-button"><i class="fa-solid fa-trash"></i></button>`; 
 
+        let deleteButton = li.querySelector('.delete-button');
+        deleteButton.addEventListener('click', function () {
+            li.remove(); 
+        });
 
-// const person={name:'John',hobbies:['reading','swimming']};
-// // const person2={...person};
-// // console.log('person', person2);
-// // const copyPerson={...person.hobbies=['reading','swimming','cooking']};
+        list.appendChild(li);
+        input.value = ''
+    }
+    else{
+        alert('Write something!')
+    }
+})
 
-// const newperson = {...person,hobbies:[...person.hobbies,'cooking']};
-// // const newperson=person;
-// console.log('newperson',newperson);
-
-
-
-const convertToObj=(['name','john'],['age',25])
-console.log(convertToObj);
 
